f17baeb209d0bede54d7aaf2eb7a5f66e1b642fd
边上海 <bianshanghai@realis-e.com>
2022-09-14 18:49:37 +0800

    删除 logs 文件，新增 一个低版本 unity需要的 attribute

---
+++ Assets/ET Network Module/Internal/ETTask/AsyncMethodBuilderAttribute.cs
@@ -0,0 +1,10 @@
+﻿#if !UNITY_2021_2_OR_NEWER
+namespace System.Runtime.CompilerServices
+{
+    public sealed class AsyncMethodBuilderAttribute : Attribute
+    {
+        public Type BuilderType { get; }
+        public AsyncMethodBuilderAttribute(Type builderType) => BuilderType = builderType;
+    }
+}
+#endif
